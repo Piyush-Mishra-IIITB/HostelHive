de47b520ea37d021d9695c8e4050c5ef29ca5bed
Piyush mishra <piyushmishra8982@gmail.com>
2026-01-22 08:48:46 +0530

    using passport for login and signup

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ const ejsMate=require("ejs-mate");
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/hostelhive');
 }
-const listings=require("./routes/listing.js");
-const reviews=require("./routes/review.js");
+const listingsRouter=require("./routes/listing.js");
+const reviewsRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
 const path=require("path");
 const ExpressError=require("./utils/ExpressError.js");
 const session=require("express-session");
-
+const passport=require("passport");
+const localStrategy=require("passport-local");
+const User=require("./models/user.js");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({ extended: true }));
@@ -46,13 +49,20 @@ cookie:{
 };
 app.use(session(secreOption));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
   res.locals.success=req.flash("success");
   res.locals.error=req.flash("error");
   next();
 });
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings",listingsRouter);
+app.use("/listings/:id/reviews",reviewsRouter);
+app.use("/",userRouter);
   
 app.use((req,res,next)=>{
     next(new ExpressError(404,"page not found"));
